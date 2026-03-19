f8849ff8a819443d262e48b32b617bb69f453e05
buithanhanhvu <buivu5944@gmail.com>
2026-03-19 11:04:24 +0700

    chore: clean repo remove cv/docker and large artifacts

---
+++ src/main/java/com/example/BuiThanhAnhVu_2280603718/BuiThanhAnhVu2280603718Application.java
@@ -0,0 +1,13 @@
+package com.example.BuiThanhAnhVu_2280603718;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuiThanhAnhVu2280603718Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuiThanhAnhVu2280603718Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BuiThanhAnhVu_2280603718/BuiThanhAnhVu2280603718ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BuiThanhAnhVu_2280603718;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuiThanhAnhVu2280603718ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
